e12256dc50d962762683d4afc67315ba100c87c2
arianejay <arianejaytantan@gmail.com>
2022-07-25 17:12:42 +0800

    7/25/2022

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://git.heroku.com/blog-it-mern.git',
+  baseURL: 'https://blog-it-mern.herokuapp.com/',
 })
